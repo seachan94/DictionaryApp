86cd94f6814fe548d00117367971849fd274962c
choesechan <seachan94@naver.com>
2021-12-25 20:07:39 +0900

    feat : 구분선 적용

--- app/src/main/java/com/example/dictionaryapp/util/RecyclerViewBindingAdapter.kt
+++ app/src/main/java/com/example/dictionaryapp/util/RecyclerViewBindingAdapter.kt
@@ -1,6 +1,8 @@
 package com.example.dictionaryapp.util
 
+import android.graphics.Color
 import android.util.Log
+import androidx.annotation.ColorInt
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -11,9 +13,24 @@ import com.example.dictionaryapp.ui.adapter.WordAdapter
 
 
 @BindingAdapter("items")
-fun <T> RecyclerView.setItems( resource: Resource<Any> ){
-    if(resource is Resource.Success){
-        val thisAdapter = if(adapter == null) WordAdapter() else adapter as WordAdapter
+fun <T> RecyclerView.setItems(resource: Resource<Any>) {
+    if (resource is Resource.Success) {
+        val thisAdapter = if (adapter == null) WordAdapter() else adapter as WordAdapter
         thisAdapter.setItem(resource.data)
     }
+}
+
+@BindingAdapter(
+    value = ["dividerHeight", "dividerPadding", "dividerColor"]
+    , requireAll = false
+)
+fun RecyclerView.setDivider(
+    dividerHeight: Float?,
+    dividerPadding: Float?,
+    @ColorInt dividerColor: Int?,
+) {
+    val decoration = Decoration(height = dividerHeight ?: 0f,
+        padding = dividerPadding ?: 0f,
+        color = dividerColor ?: Color.TRANSPARENT)
+    addItemDecoration (decoration)
 }
